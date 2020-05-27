6fbdee6f622535179b64f393cf34133d4bfbb298
samg2226 <gallagher.psu@gmail.com>
2020-05-27 16:56:50 -0400

    zip, gzip, and bzip unzipping and name change is done now we need to add what the comments say

--- Psuedo_Extract.py
+++ Psuedo_Extract.py
@@ -2,23 +2,61 @@ def zip():
     zip_archive = zipfile.ZipFile(filename, 'r')
     zip_archive.extractall(path=os.path.join(os.getcwd()) + '/extracted_zip')
 
+    """this creates another directory and puts the file in that directory"""
     """move the file back into the other directory"""
 
 def bzip():
-    """unzip the file
-       move it back to the right dir
-       change the filename"""
+    """have python check if there is a directory named extracted_zip and if there is then go
+       into that directory and then run the code below, MAYBE DO THIS TWICE BECAUSE THERE COULD BE
+       ZIP FILES IN THE EXTRACTED_ZIP DIRECTORY"""
+
+
+    line_list = []
+    file = bz2file.open(filename, 'r')
+    for line in file.readlines():
+        line = str(line)
+        line = line.replace('b', '')
+        line = line[1:-3]
+        line_list.append(line)
+    filename = filename[0:-4]
+    f = open(filename, 'w')
+    new_file = '\n'.join(line_list)
+    f.write(new_file)
+    f.close()
+
+
+
+    """move it back to the right dir"""
 
 def gzip():
-    """unzip the file
-        move it back to the right dir
-        change the filename"""
+    """have python check if there is a directory named extracted_zip and if there is then go
+       into that directory and then run the code below, MAYBE DO THIS TWICE BECAUSE THERE COULD BE
+       ZIP FILES IN THE EXTRACTED_ZIP DIRECTORY"""
+
+    line_list = []
+    import gzip
+    f = gzip.open(filename, 'rb')
+    for line in f.readlines():
+        line = str(line)
+        line = line.replace('b', '')
+        line = line[1:-3]
+        line_list.append(line)
+    filename = filename[0:-3]
+    f = open(filename, 'w')
+    new_file = '\n'.join(line_list)
+    f.write(new_file)
+    f.close()
+
+
+
 
 
 
 import magic
 import zipfile
 import os
+import bz2file
+import gzip
 
 def main():
     global filename
@@ -43,7 +81,7 @@ def main():
         elif Current_ENC[0:3] == 'zip':
             zip()
 
-    return 'success the file is in {}'.format(os.getcwd())
+    return 'success the file(s) is in {}'.format(os.getcwd())
 
 
 
